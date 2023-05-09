785af2487d153162ac9b3812e10d54d1034705d4
VeroAlvarez09 <100813865+VeroAlvarez09@users.noreply.github.com>
2023-05-08 19:11:27 -0500

    Se inicializa proyecto

---
+++ app/src/main/java/com/app/flyapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.flyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
